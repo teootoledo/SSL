889dbce4fb4a7ca1fe7dec0eb9aa9f7e349a5f54
Teo Martin Toledo <teootoledo@gmail.com>
2020-06-20 17:12:08 -0300

    Agregadas precondiciones de funciones

--- 04-Strings/String.h
+++ 04-Strings/String.h
@@ -8,14 +8,19 @@ Teo Martin Toledo
 #include <stdbool.h>
 //--------------------------------
 
+
 //GetLength: Σ* → |N
+//La cadena de caracteres debe ser válida y terminar con '\0'.
 unsigned GetLength(const char[]);
 
 //IsEmpty: Σ* → |B
+//Sin precondiciones.
 bool IsEmpty(const char[]);
 
 //Power: Σ* x |N → Σ*
+//Se asume el último argumento tiene la capacidad para almacenar la totalidad de la operación.
 void Power(const char[], const unsigned, char[]);
 
 //Contains Σ* x Σ → |B
+//Se debe pasar 0 como tercer argumento. Esto debido a que no existen argumentos por defecto en C.
 bool Contains(const char[], const char, unsigned pos);
