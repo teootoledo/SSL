7be819c93d2c25c6d1f348f0febb0b9ecd69f4c2
Teo Martin Toledo <teootoledo@gmail.com>
2020-09-14 15:52:57 -0300

    Parser terminado

--- 06-CalcInfManual/parser.c
+++ 06-CalcInfManual/parser.c
@@ -1,14 +1,10 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
 #include "scanner.h"
 #include "parser.h"
 
 typedef enum
 {
     Q0_inicio,
-    Q1_final,
-    Q2_err
+    Q1_final
 } EstadoParser;
 
 bool parser(void)
@@ -20,25 +16,23 @@ bool parser(void)
         switch (estadoActual)
         {
         case Q0_inicio:
-            if (newToken != identificador || newToken != constante)
+            if ((newToken == identificador) || (newToken == constante))
             {
-                estadoActual = Q2_err;
+                estadoActual = Q1_final;
+                break;
             }
-            estadoActual = Q1_final;
-            break;
+            return false;
         case Q1_final:
-            if (newToken != adicion || newToken != producto)
+            if ((newToken == adicion) || (newToken == producto))
             {
-                estadoActual = Q2_err;
+                estadoActual = Q0_inicio;
+                break;
             }
-            estadoActual = Q0_inicio;
-        case Q2_err:
-            printf("Syntax ERROR");
-            exit(1);
-            break;
-
-        default:
-            break;
+            return false;
         }
+    }
+    if (estadoActual == Q0_inicio)
+    {
+        return false;
     }
 }
